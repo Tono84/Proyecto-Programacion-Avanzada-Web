78085e08ef2b58c40ca216bb64cd8ef6a0fed166
Mateo Esquivel <mateo.esro1@gmail.com>
2023-11-30 11:44:56 -0600

    Pagina de membresia

---
+++ frontend-razor/frontend-razor/Controllers/MembershipController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace frontend.Controllers
+{
+    public class MembershipController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Confirmation() 
+        {
+            return View();
+        }
+
+    }
+}
